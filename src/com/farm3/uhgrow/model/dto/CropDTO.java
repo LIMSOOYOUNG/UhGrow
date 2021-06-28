b75f273cbf93ed13772abf2fcec6033a062ddb6b
Bongseok Choi <tbdi3751@gmail.com>
2021-06-28 23:56:38 +0900

    FoodDTO, CropDTO, RetainFoodDTO 추가

---
+++ src/com/farm3/uhgrow/model/dto/CropDTO.java
@@ -0,0 +1,49 @@
+package com.farm3.uhgrow.model.dto;
+
+public class CropDTO {
+	
+	private int cropId;       // 농작물아이디
+	private String cropName;  // 농작물이름
+	private int cropPrice;    // 농작물가격
+	
+	public CropDTO() {}
+
+	public CropDTO(int cropId, String cropName, int cropPrice) {
+		super();
+		this.cropId = cropId;
+		this.cropName = cropName;
+		this.cropPrice = cropPrice;
+	}
+
+	public int getCropId() {
+		return cropId;
+	}
+
+	public String getCropName() {
+		return cropName;
+	}
+
+	public int getCropPrice() {
+		return cropPrice;
+	}
+
+	public void setCropId(int cropId) {
+		this.cropId = cropId;
+	}
+
+	public void setCropName(String cropName) {
+		this.cropName = cropName;
+	}
+
+	public void setCropPrice(int cropPrice) {
+		this.cropPrice = cropPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "CropDTO [cropId=" + cropId + ", cropName=" + cropName + ", cropPrice=" + cropPrice + "]";
+	}
+
+	
+
+}
