78da72d873861b43d87445bc04cee3eada8c4b33
BSC <BSC@DESKTOP-6U1CIK9>
2021-07-01 02:31:41 +0900

    관리자모드 메인, 회원관리 메인, 계정조회및수정, 계정삭제, 계정복구 구현중

--- src/com/farm3/uhgrow/management/run/MngRun.java
+++ src/com/farm3/uhgrow/management/run/MngRun.java
@@ -1,5 +1,6 @@
 package com.farm3.uhgrow.management.run;
 
+import com.farm3.uhgrow.management.view.MngMoniterView;
 import com.farm3.uhgrow.management.view.MngView;
 
 public class MngRun {
@@ -7,7 +8,9 @@ public class MngRun {
 	public static void main(String[] args) {
 		
 		MngView mng = new MngView();
-		mng.mngMenu();
+//		mng.mngMenu();
+		new MngMoniterView();
+
 
 	}
 
