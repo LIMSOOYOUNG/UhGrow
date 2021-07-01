4574ddfdb77226130e9ca885e8ea12167490fa7c
82106 <82106@DESKTOP-UG1UE1J>
2021-07-01 10:37:48 +0900

    210630-donggi-2입니다 1로잘못적었어요

--- src/com/farm3/uhgrow/member/view/LoginFrame.java
+++ src/com/farm3/uhgrow/member/view/LoginFrame.java
@@ -22,9 +22,12 @@ import com.farm3.uhgrow.member.controller.MemberController;
 import com.farm3.uhgrow.member.model.dto.UserDTO;
 
 public class LoginFrame extends JFrame {
-
+	Font font = new Font("고딕체", Font.BOLD, 30);
 
 	public LoginFrame() {
+		loginPanel();
+	}
+	public void loginPanel() {
 		this.setBounds(300, 200, 960, 565);
 		this.setTitle("UhGrow");
 		this.setLayout(null);
@@ -32,7 +35,7 @@ public class LoginFrame extends JFrame {
 		// 패널 생성 
 		JPanel backGroundPanel = new JPanel();
 		JPanel loginPanel = new JPanel();
-		Font font = new Font("Arial", Font.BOLD, 30);
+		
 
 		// backGroundPanel위치 지정 후  BackGrountLabel(이미지) 삽입
 		loginPanel.setLayout(null);
@@ -102,7 +105,7 @@ public class LoginFrame extends JFrame {
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		
+
 		loginButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {String loginId = idField.getText().toString();
@@ -132,9 +135,13 @@ public class LoginFrame extends JFrame {
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
 
+				loginPanel.removeAll();
+				loginPanel.setVisible(false);
+				signUpPanel();
+
 			}
 		});
-		
+
 		findIdPwdButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -142,10 +149,98 @@ public class LoginFrame extends JFrame {
 
 			}
 		});
-
-		
 	}
 
+	public void signUpPanel() {
+
+		JPanel signUpPanel = new JPanel();
+
+		signUpPanel.setLayout(null);
+		signUpPanel.setSize(960,540);
+		Image background = new ImageIcon("img/interface/backGround1.png").getImage().getScaledInstance(960, 540, 0);
+		JLabel backGroundLabel = new JLabel(new ImageIcon(background));
+		backGroundLabel.setLocation(0, 0);
+		backGroundLabel.setSize(960, 540);
+		
+		Image signUpBackground = new ImageIcon("img/loginInterface/signUp.png").getImage().getScaledInstance(230, 72, 0);
+		JButton signUpButton = new JButton(new ImageIcon(signUpBackground));
+		signUpButton.setLayout(null);
+		signUpButton.setBounds(240,340,230,72);
+		
+		
+		Image backButtonBackground = new ImageIcon("img/loginInterface/back.png").getImage().getScaledInstance(230, 72, 0);
+		JButton backButton = new JButton(new ImageIcon(backButtonBackground));
+		backButton.setLayout(null);
+		backButton.setBounds(490,340,230,72);
+		
+		JLabel idLabel = new JLabel("I   D   : ");
+		idLabel.setLayout(null);
+		idLabel.setBounds(300, 60, 100, 50);
+		idLabel.setFont(font);
+
+		JTextField idField = new JTextField();
+		idField.setLayout(null);
+		idField.setBounds(400, 60, 300, 50);
+		idField.setFont(font);
+		
+		JLabel pwdLabel = new JLabel("P  W  D  :  ");
+		pwdLabel.setLayout(null);
+		pwdLabel.setBounds(259, 125, 200, 50);
+		pwdLabel.setFont(font);
+
+		JPasswordField pwdField = new JPasswordField();
+		pwdField.setLayout(null);
+		pwdField.setBounds(400, 125, 300, 50);
+		pwdField.setFont(font);
+		
+		JLabel nameLabel = new JLabel("N A M E : ");
+		nameLabel.setLayout(null);
+		nameLabel.setBounds(259, 190, 200, 50);
+		nameLabel.setFont(font);
+		
+		JTextField nameField = new JTextField();
+		nameField.setLayout(null);
+		nameField.setBounds(400, 190, 300, 50);
+		nameField.setFont(font);
+		
+		JLabel mailLabel = new JLabel("E - MAIL: ");
+		mailLabel.setLayout(null);
+		mailLabel.setBounds(259, 255, 200, 50);
+		mailLabel.setFont(font);
+		
+		JTextField mailField = new JTextField();
+		mailField.setLayout(null);
+		mailField.setBounds(400, 255, 300, 50);
+		mailField.setFont(font);
+
+		signUpPanel.add(idLabel);
+		signUpPanel.add(idField);
+		signUpPanel.add(pwdLabel);
+		signUpPanel.add(pwdField);
+		signUpPanel.add(nameLabel);
+		signUpPanel.add(nameField);
+		signUpPanel.add(mailLabel);
+		signUpPanel.add(mailField);
+		
+		
+		signUpPanel.add(backButton);
+		signUpPanel.add(signUpButton);
+		signUpPanel.add(backGroundLabel);
+		
+		this.add(signUpPanel);
+		
+		backButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				signUpPanel.removeAll();
+				signUpPanel.setVisible(false);
+				loginPanel();
+
+
+			}
+		});
+	}
 }
 
 
