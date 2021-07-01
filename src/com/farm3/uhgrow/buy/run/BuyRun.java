c0c6cb760093805b33a1fe3e085cca8867a47f77
Chanbin <62756484+chanbin0510@users.noreply.github.com>
2021-07-01 23:35:55 +0900

    chanbin frame 스윙 또 만나면 뒤진다 ㄹㅇ

---
+++ src/com/farm3/uhgrow/buy/run/BuyRun.java
@@ -0,0 +1,13 @@
+package com.farm3.uhgrow.buy.run;
+
+import com.farm3.uhgrow.buy.view.BuyFrameView;
+
+public class BuyRun {
+	
+	public static void main(String[] args) {
+		
+		new BuyFrameView();
+		
+	}
+
+}
